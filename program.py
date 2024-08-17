9c4dbf19b7f4fee6ff1a9e1df3884fa9743e76b0
durgadevi22d <durgadevi22d@gmail.com>
2024-08-17 09:20:53 +0530

    Sucess

---
+++ program.py
@@ -0,0 +1,28 @@
+from http.server import HTTPServer,BaseHTTPRequestHandler
+
+content='''
+<!doctype html>
+<html>
+<head>
+<title> My Web Server </title>
+</head>
+<body>
+<h1>Hello! This is Durgadevi...</h1>
+<h2>Welcome to my first webpage</h2>
+<t><h2> See u soon.....</h2></t>
+</body>
+</html>
+'''
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("Get request received...")
+        self.send_response(200) 
+        self.send_header("content-type", "text/html")       
+        self.end_headers()
+        self.wfile.write(content.encode())
+
+print("This is my webserver") 
+server_address =('',8000)
+httpd = HTTPServer(server_address,MyServer)
+httpd.serve_forever()
